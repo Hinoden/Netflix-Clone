f7b7f277fc85b32db253b90b1ff2a2487ce39f95
Hinoden <krwong3@gmail.com>
2025-01-17 17:21:20 -0800

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import authRoutes from "./routes/auth.route.js";
+import {ENV_VARS} from './config/envVars.js';
+import {connectDB} from './config/db.js';
+
+const app = express();
+const PORT = ENV_VARS.PORT
+app.use(express.json());        //allows us to parse req.body
+
+//connect to the auth routes depending on which page is visited
+app.use("/api/v1/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log("Server started at http://localhost:" + PORT);
+    connectDB();
+});
