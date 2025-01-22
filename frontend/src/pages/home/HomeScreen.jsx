552260908d8b456aad0a0c332db3894a0e791a38
Hinoden <krwong3@gmail.com>
2025-01-21 19:28:35 -0800

    Completed signin, login, and logout functionality

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,6 +1,12 @@
+import {useAuthStore} from "../../store/authUser.js"
+
 const HomeScreen = () => {
+    const { logout } = useAuthStore();
     return (
-        <div>HomeScreen</div>
+        <div>
+            HomeScreen
+            <button onClick={logout}>Logout</button>
+        </div>
     )
 }
 export default HomeScreen
