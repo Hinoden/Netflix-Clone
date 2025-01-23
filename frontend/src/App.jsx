bcab8e84273b25b8714a21e523a58d7bcf60a4ab
Hinoden <krwong3@gmail.com>
2025-01-22 19:38:14 -0800

    Completed 404 error page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SignUpPage from './pages/SignUpPage.jsx';
 import WatchPage from './pages/WatchPage.jsx';
 import SearchPage from './pages/SearchPage.jsx';
 import SearchHistoryPage from './pages/SearchHistoryPage.jsx'
+import NotFoundPage from './pages/404.jsx';
 import Footer from './components/Footer.jsx';
 import { Toaster } from 'react-hot-toast';
 import {useAuthStore} from './store/authUser';
@@ -37,6 +38,7 @@ function App() {
         <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
         <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
         <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+        <Route path='/*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
       <Toaster />
