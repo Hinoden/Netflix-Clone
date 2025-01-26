e1aad7228089ccc87f67797f29f705c0f663f831
Hinoden <krwong3@gmail.com>
2025-01-26 01:37:12 -0800

    testing api --> backend name change

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -43,7 +43,7 @@ export const useAuthStore = create((set) => ({
     authCheck: async () => {
         set({isCheckingAuth: true});
         try{
-            const response = await axios.get("https://netflix-clone-black-two.vercel.app/api/v1/auth/authCheck");
+            const response = await axios.get("https://netflix-clone-black-two.vercel.app/backend/v1/auth/authCheck");
             set({user: response.data.user, isCheckingAuth: false});
         } catch (error){
             set({ isCheckingAuth: false, user: null});
