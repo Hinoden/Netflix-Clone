cc083b0666a8d3dc03352443d59f11c59a124a50
Hinoden <krwong3@gmail.com>
2025-01-30 22:55:13 -0800

    Fixing incorrect routing

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -11,7 +11,7 @@ export const useAuthStore = create((set) => ({
     signup: async (credentials) => {
         set({isSigningUp:true});
         try {
-            const response = await axios.post("https://netflix-clone-api-xi.vercel.app/api/auth/signup", credentials);
+            const response = await axios.post("https://netflix-clone-api-xi.vercel.app/api/v1/auth/signup", credentials);
             set({user:response.data.user, isSigningUp: false});
             toast.success("Account created successfully");
         } catch (error) {
@@ -22,7 +22,7 @@ export const useAuthStore = create((set) => ({
     login: async (credentials) => {
         set({isLoggingIn: true});
         try {
-            const response = await axios.post("https://netflix-clone-api-xi.vercel.app/api/auth/login", credentials);
+            const response = await axios.post("https://netflix-clone-api-xi.vercel.app/api/v1/auth/login", credentials);
             set({user: response.data.user, isLoggingIn: false});
         } catch (error){
             set({ isLoggingIn: false, user: null});
