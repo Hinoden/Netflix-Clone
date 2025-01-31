0903f29eee585e6551447976f63ff52cd3304d44
Hinoden <krwong3@gmail.com>
2025-01-30 23:22:56 -0800

    Fixed routes

--- frontend/src/hooks/useGetTrendingContent.jsx
+++ frontend/src/hooks/useGetTrendingContent.jsx
@@ -8,7 +8,7 @@ const useGetTrendingContent = () => {
 
     useEffect(() => {
         const getTrendingContent = async () => {
-            const res = await axios.get(`https://netflix-clone-api-xi.vercel.api/api/${contentType}/trending`);
+            const res = await axios.get(`https://netflix-clone-api-xi.vercel.app/api/v1/${contentType}/trending`);
             setTrendingContent(res.data.content);
         };
 
