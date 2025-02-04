ca0be39179f2df25611843830fd8c86a6680e594
Hinoden <krwong3@gmail.com>
2025-02-03 18:37:01 -0800

    testing with overflow code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     proxy:{
+//       '/api': {
+//         target: 'https://netflix-clone-api-xi.vercel.app',
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api': {
-        target: 'https://netflix-clone-api-xi.vercel.app',
-      }
-    }
+  server: {
+    proxy: {
+      "/proxy": {
+        target: "https://netflix-clone-api-xi.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy/, '')
+      },
+    },
   }
 })
