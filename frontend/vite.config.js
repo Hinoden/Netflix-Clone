4034cdbd6b5b83fa7fb638a874cf93352eeff75e
Hinoden <krwong3@gmail.com>
2025-02-03 20:00:42 -0800

    testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,17 +17,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://netflix-clone-api-xi.vercel.app',
+      "/api": {
+        target: "https://netflix-clone-api-xi.vercel.app",
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+        secure: true,  // Ensure SSL verification
+        rewrite: (path) => path.replace(/^\/api/, '') // Removes `/api` prefix before forwarding
+      },
+    },
+  },
 });
