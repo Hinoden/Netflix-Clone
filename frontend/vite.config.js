1558c4dbae191b1fa4cfb9ab75415ad92ff0d747
Hinoden <krwong3@gmail.com>
2025-02-03 19:27:49 -0800

    resetting vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,42 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://netflix-clone-api-xi.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"), // Keeps "/api"
-      },
-    },
-  },
-});
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     proxy:{
-//       '/api': {
-//         target: 'https://netflix-clone-api-xi.vercel.app',
-//       }
-//     }
-//   }
-// })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/proxy": {
-//         target: "https://netflix-clone-api-xi.vercel.app",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/proxy/, '')
-//       },
-//     },
-//   }
-// })
+  server:{
+    proxy:{
+      '/api': {
+        target: 'https://netflix-clone-api-xi.vercel.app',
+      }
+    }
+  }
+})
