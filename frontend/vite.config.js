42cc11ed29e99d894bac91155e7e84786f5422f7
Hinoden <krwong3@gmail.com>
2025-02-05 19:47:33 -0800

    fixed code to work for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,14 @@
-//original code
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     proxy:{
-//       '/api': {
-//         target: 'https://netflix-clone-api-xi.vercel.app',
-//       }
-//     }
-//   }
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
+  server:{
+    proxy:{
       '/api': {
-        target: 'https://netflix-clone-api-xi.vercel.app',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        target: 'http://localhost:3500',
       }
     }
   }
-});
+})
